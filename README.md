13a9191513d93972357260221f4f396d82727a4f
CrazyMax <crazy-max@users.noreply.github.com>
2017-11-19 21:44:55 +0100

    Move repository to Portapps org

--- README.md
+++ README.md
@@ -1,36 +1,44 @@
-<p align="center"><a href="https://github.com/crazy-max/HostsManPortable" target="_blank"><img width="100" src="https://github.com/crazy-max/HostsManPortable/blob/master/res/logo.png"></a></p>
+<p align="center"><a href="https://github.com/portapps/hostsman-portable" target="_blank"><img width="100" src="https://github.com/portapps/hostsman-portable/blob/master/res/papp.png"></a></p>
 
 <p align="center">
-  <a href="https://github.com/crazy-max/HostsManPortable/releases/latest"><img src="https://img.shields.io/github/release/crazy-max/HostsManPortable.svg?style=flat-square" alt="GitHub release"></a>
-  <a href="https://github.com/crazy-max/HostsManPortable/releases/latest"><img src="https://img.shields.io/github/downloads/crazy-max/HostsManPortable/total.svg?style=flat-square" alt="Total downloads"></a>
-  <a href="https://ci.appveyor.com/project/crazy-max/HostsManPortable"><img src="https://img.shields.io/appveyor/ci/crazy-max/HostsManPortable.svg?style=flat-square" alt="AppVeyor"></a>
-  <a href="https://goreportcard.com/report/github.com/crazy-max/HostsManPortable"><img src="https://goreportcard.com/badge/github.com/crazy-max/HostsManPortable?style=flat-square" alt="Go Report"></a>
-  <a href="https://www.codacy.com/app/crazy-max/HostsManPortable"><img src="https://img.shields.io/codacy/grade/f8c77f7d45d34409b16d3b957ef80cf7.svg?style=flat-square" alt="Code Quality"></a>
-  <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=EZWH7N7DYDECQ"><img src="https://img.shields.io/badge/donate-paypal-blue.svg?style=flat-square" alt="Donate Paypal"></a>
-  <a href="https://flattr.com/submit/auto?user_id=crazymax&url=https://github.com/crazy-max/HostsManPortable"><img src="https://img.shields.io/badge/flattr-this-green.svg?style=flat-square" alt="Flattr this!"></a>
+  <a href="https://github.com/portapps/hostsman-portable/releases/latest"><img src="https://img.shields.io/github/release/portapps/hostsman-portable.svg?style=flat-square" alt="GitHub release"></a>
+  <a href="https://github.com/portapps/hostsman-portable/releases/latest"><img src="https://img.shields.io/github/downloads/portapps/hostsman-portable/total.svg?style=flat-square" alt="Total downloads"></a>
+  <a href="https://ci.appveyor.com/project/portapps/hostsman-portable"><img src="https://img.shields.io/appveyor/ci/crazy-max/hostsman-portable.svg?style=flat-square" alt="AppVeyor"></a>
+  <a href="https://goreportcard.com/report/github.com/portapps/hostsman-portable"><img src="https://goreportcard.com/badge/github.com/portapps/hostsman-portable?style=flat-square" alt="Go Report"></a>
+  <a href="https://www.codacy.com/app/portapps/hostsman-portable"><img src="https://img.shields.io/codacy/grade/f8c77f7d45d34409b16d3b957ef80cf7.svg?style=flat-square" alt="Code Quality"></a>
+  <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WQD7AQGPDEPSG"><img src="https://img.shields.io/badge/donate-paypal-7057ff.svg?style=flat-square" alt="Donate Paypal"></a>
 </p>
 
 ## About
 
-A single EXE written in [Go](https://golang.org/) to make [HostsMan](http://www.abelhadigital.com/hostsman) portable on Windows systems.<br />
+[HostsMan](http://www.abelhadigital.com/hostsman) portable app made with 🚀 [Portapps](https://github.com/portapps).<br />
 Tested on Windows 7, Windows 8.1 and Windows 10.
 
 ## Installation
 
 There are different kinds of artifacts :
 
-* `HostsManPortable-x.x.x-x-setup.exe` : Full portable release of HostsMan as a setup. **Recommended way**!
-* `HostsManPortable-x.x.x-x.7z` : Full portable release of HostsMan as a 7z archive.
-* `HostsManPortable.exe` : Only the portable binary (must be dropped in the root folder near `hm.exe`)
-* `HostsMan_x.x.x.exe` : The original release from the [official website](http://www.abelhadigital.com/hostsman).
+* `hostsman-portable-{ia32,x64}-x.x.x-x-setup.exe` : Full portable release of Brave as a setup. **Recommended way**!
+* `hostsman-portable-{ia32,x64}-x.x.x-x.7z` : Full portable release of Brave as a 7z archive.
+* `hostsman-portable-{ia32,x64}.exe` : Only the portable binary (must be renamed `hostsman-portable.exe`)
+* `HostsMan_{ia32,x64}_x.x.x.zip` : The original release from the [official website](http://www.abelhadigital.com/hostsman).
 
-For a **fresh installation**, install `HostsManPortable-x.x.x-x-setup.exe` where you want then run `HostsManPortable.exe`.
+### Fresh installation
 
-If **you have already installed HostsMan from the original setup or zip file**, do the same thing as a fresh installation and run `HostsManPortable.exe` a first time.<br />
-The data located in `%APPDATA%\abelhadigital.com\HostsMan` and `%PROGRAMDATA%\abelhadigital.com\HostsMan` will be moved to their respectives folders.<br />
-Then you can remove HostsMan from your computer.
+Install `hostsman-portable-{ia32,x64}-x.x.x-x-setup.exe` where you want then run `hostsman-portable.exe`.
 
-**For an upgrade**, simply download and install the [latest setup](https://github.com/crazy-max/HostsManPortable/releases/latest).
+### App already installed
+
+If **you have already installed HostsMan from the original release**, do the same thing as a fresh installation and move :
+
+* `%APPDATA%\abelhadigital.com\HostsMan` to `data\UserProfile\AppData\Roaming\abelhadigital.com\HostsMan`
+* `%PROGRAMDATA%\abelhadigital.com\HostsMan` to `data\ProgramData\abelhadigital.com\HostsMan`
+
+Run `hostsman-portable.exe` and then you can [remove](https://support.microsoft.com/en-us/instantanswers/ce7ba88b-4e95-4354-b807-35732db36c4d/repair-or-remove-programs) Brave from your computer.
+
+### Upgrade
+
+**For an upgrade**, simply download and install the [latest hostsman-portable setup](https://github.com/portapps/hostsman-portable/releases/latest).
 
 ## How can i help ?
 
@@ -38,15 +46,9 @@ We welcome all kinds of contributions :raised_hands:!<br />
 The most basic way to show your support is to star :star2: the project, or to raise issues :speech_balloon:<br />
 Any funds donated will be used to help further development on this project! :gift_heart:
 
-<p>
-  <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=EZWH7N7DYDECQ">
-    <img src="https://github.com/crazy-max/HostsManPortable/blob/master/res/paypal.png" alt="Donate Paypal">
-  </a>
-  <a href="https://flattr.com/submit/auto?user_id=crazymax&url=https://github.com/crazy-max/HostsManPortable">
-    <img src="https://github.com/crazy-max/HostsManPortable/blob/master/res/flattr.png" alt="Flattr this!">
-  </a>
-</p>
+[![Donate Paypal](https://raw.githubusercontent.com/portapps/portapps/master/res/paypal.png)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WQD7AQGPDEPSG)
 
 ## License
 
-MIT. See `LICENSE` for more details.
+MIT. See `LICENSE` for more details.<br />
+Rocket icon credit to [Squid Ink](http://thesquid.ink).
--- main.go
+++ main.go
@@ -1,276 +1,34 @@
 //go:generate go get -v github.com/josephspurrier/goversioninfo/...
-//go:generate goversioninfo -icon=res/app.ico
+//go:generate goversioninfo -icon=res/papp.ico -manifest=res/papp.manifest
 package main
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"path"
-	"path/filepath"
-	"strings"
-	"syscall"
-
-	"github.com/op/go-logging"
+	. "github.com/portapps/portapps"
 )
 
-const (
-	NAME                 = "HostsManPortable"
-	APP_NAME             = "HostsMan"
-	APP_DATA_BASE_FOLDER = "abelhadigital.com"
-	APP_DATA_FOLDER      = "HostsMan"
-	APP_PROCESS          = "hm.exe"
-)
-
-var (
-	log       = logging.MustGetLogger(NAME)
-	logFormat = logging.MustStringFormatter(`%{time:2006-01-02 15:04:05} %{level:.4s} - %{message}`)
-)
+func init() {
+	App.ID = "hostsman-portable"
+	App.Name = "HostsMan"
+	Init()
+}
 
 func main() {
-	// Current path
-	currentPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		log.Error("Current path:", err)
-	}
+	//App.MainPath =
+	App.DataPath = CreateFolder(App.DataPath)
+	App.Process = RootPathJoin("hm.exe")
+	App.Args = nil
+	App.WorkingDir = App.MainPath
 
-	// Log file
-	logfile, err := os.OpenFile(path.Join(currentPath, NAME+".log"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		log.Error("Log file:", err)
-	}
+	// Create data folders
+	progDataPath := CreateFolder(PathJoin(App.RootDataPath, "ProgramData", "abelhadigital.com", "HostsMan"))
+	userProfilePath := CreateFolder(PathJoin(App.RootDataPath, "UserProfile", "AppData", "Roaming", "abelhadigital.com", "HostsMan"))
+	Log.Infof("Prog data path: %s", progDataPath)
+	Log.Infof("User profile path: %s", userProfilePath)
 
-	// Init logger
-	logBackendStdout := logging.NewBackendFormatter(logging.NewLogBackend(os.Stdout, "", 0), logFormat)
-	logBackendFile := logging.NewBackendFormatter(logging.NewLogBackend(logfile, "", 0), logFormat)
-	logging.SetBackend(logBackendStdout, logBackendFile)
-	log.Info("--------")
-	log.Info("Starting " + NAME + "...")
-	log.Info("Current path:", currentPath)
-
-	// Init vars
-	var exe = path.Join(currentPath, APP_PROCESS)
-	var progdataPath = path.Join(currentPath, "progdata")
-	var appdataPath = path.Join(currentPath, "appdata")
-	var symlinkBaseProgdataPath = path.Clean(path.Join(os.Getenv("PROGRAMDATA"), APP_DATA_BASE_FOLDER))
-	var symlinkProgdataPath = path.Clean(path.Join(symlinkBaseProgdataPath, APP_DATA_FOLDER))
-	var symlinkBaseAppdataPath = path.Clean(path.Join(os.Getenv("APPDATA"), APP_DATA_BASE_FOLDER))
-	var symlinkAppdataPath = path.Clean(path.Join(symlinkBaseAppdataPath, APP_DATA_FOLDER))
-	log.Info("Executable:", exe)
-	log.Info("Progdata path:", progdataPath)
-	log.Info("Appdata path:", appdataPath)
-	log.Info("Symlink Progdata path:", symlinkProgdataPath)
-	log.Info("Symlink Appdata path:", symlinkAppdataPath)
-
-	// Create progdata folder
-	if _, err := os.Stat(progdataPath); os.IsNotExist(err) {
-		log.Info("Create progdata folder...", progdataPath)
-		err = os.Mkdir(progdataPath, 777)
-		if err != nil {
-			log.Error("Create progdata folder:", err)
-		}
-	}
-
-	// Create appdata folder
-	if _, err := os.Stat(appdataPath); os.IsNotExist(err) {
-		log.Info("Create appdata folder...", appdataPath)
-		err = os.Mkdir(appdataPath, 777)
-		if err != nil {
-			log.Error("Create appdata folder:", err)
-		}
-	}
-
-	// Check old progdata folder
-	if _, err := os.Stat(symlinkBaseProgdataPath); os.IsNotExist(err) {
-		log.Info("Create base progdata folder...", symlinkBaseProgdataPath)
-		err = os.Mkdir(symlinkBaseProgdataPath, 777)
-		if err != nil {
-			log.Error("Create base progdata folder:", err)
-		}
-	}
-	if _, err := os.Stat(symlinkProgdataPath); err == nil {
-		fi, err := os.Lstat(symlinkProgdataPath)
-		if err != nil {
-			log.Error("Symlink lstat:", err)
-		}
-		if fi.Mode()&os.ModeSymlink != os.ModeSymlink {
-			// Copy old data folder
-			log.Info("Copy old data from", symlinkProgdataPath)
-			err = copyDir(symlinkProgdataPath, progdataPath)
-			if err != nil {
-				log.Error("Copying old data folder:", err)
-			}
-
-			// Rename old data folder
-			log.Info("Chmod old data folder...")
-			err = os.Chmod(symlinkProgdataPath, 0777)
-			if err != nil {
-				log.Error("Chmod old data folder:", err)
-			}
-
-			log.Info("Rename old data folder to", symlinkProgdataPath+"_old")
-			err = os.Rename(symlinkProgdataPath, symlinkProgdataPath+"_old")
-			if err != nil {
-				log.Error("Renaming old data folder:", err)
-			}
-		}
-	}
-
-	// Check old appdata folder
-	if _, err := os.Stat(symlinkBaseAppdataPath); os.IsNotExist(err) {
-		log.Info("Create base appdata folder...", symlinkBaseAppdataPath)
-		err = os.Mkdir(symlinkBaseAppdataPath, 777)
-		if err != nil {
-			log.Error("Create base appdata folder:", err)
-		}
-	}
-	if _, err := os.Stat(symlinkAppdataPath); err == nil {
-		fi, err := os.Lstat(symlinkAppdataPath)
-		if err != nil {
-			log.Error("Symlink lstat:", err)
-		}
-		if fi.Mode()&os.ModeSymlink != os.ModeSymlink {
-			// Copy old data folder
-			log.Info("Copy old data from", symlinkAppdataPath)
-			err = copyDir(symlinkAppdataPath, appdataPath)
-			if err != nil {
-				log.Error("Copying old data folder:", err)
-			}
-
-			// Rename old data folder
-			log.Info("Chmod old data folder...")
-			err = os.Chmod(symlinkAppdataPath, 0777)
-			if err != nil {
-				log.Error("Chmod old data folder:", err)
-			}
-
-			log.Info("Rename old data folder to", symlinkAppdataPath+"_old")
-			err = os.Rename(symlinkAppdataPath, symlinkAppdataPath+"_old")
-			if err != nil {
-				log.Error("Renaming old data folder:", err)
-			}
-		}
-	}
-
-	// Create progdata symlink
-	log.Info("Create symlink", symlinkProgdataPath)
-	os.Remove(symlinkProgdataPath)
-	cmd := exec.Command("cmd", "/c", "mklink", "/J", strings.Replace(symlinkProgdataPath, "/", "\\", -1), strings.Replace(progdataPath, "/", "\\", -1))
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-	if err := cmd.Run(); err != nil {
-		log.Error("Symlink:", err)
-	}
-
-	// Create appdata symlink
-	log.Info("Create symlink", symlinkAppdataPath)
-	os.Remove(symlinkAppdataPath)
-	cmd = exec.Command("cmd", "/c", "mklink", "/J", strings.Replace(symlinkAppdataPath, "/", "\\", -1), strings.Replace(appdataPath, "/", "\\", -1))
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-	if err := cmd.Run(); err != nil {
-		log.Error("Symlink:", err)
-	}
-
-	// Launch
-	log.Infof("Launch %s...", APP_NAME)
-	cmd = exec.Command(exe)
-	cmd.Dir = currentPath
-
-	defer logfile.Close()
-	cmd.Stdout = logfile
-	cmd.Stderr = logfile
-
-	if err := cmd.Start(); err != nil {
-		log.Error("Cmd Start:", err)
-	}
-
-	cmd.Wait()
-}
-
-// src : https://gist.github.com/crazy-max/e50ee72138bb184baf8d1b6e81983f13
-func copyDir(src string, dst string) (err error) {
-	src = filepath.Clean(src)
-	dst = filepath.Clean(dst)
-
-	si, err := os.Stat(src)
-	if err != nil {
-		return err
-	}
-	if !si.IsDir() {
-		return fmt.Errorf("src is not a directory: %s", src)
-	}
-
-	_, err = os.Stat(dst)
-	if err != nil && !os.IsNotExist(err) {
-		return
-	}
-
-	err = os.MkdirAll(dst, si.Mode())
-	if err != nil {
-		return
-	}
-
-	entries, err := ioutil.ReadDir(src)
-	if err != nil {
-		return
-	}
-
-	for _, entry := range entries {
-		srcPath := filepath.Join(src, entry.Name())
-		dstPath := filepath.Join(dst, entry.Name())
-		if entry.IsDir() {
-			err = copyDir(srcPath, dstPath)
-			if err != nil {
-				return
-			}
-		} else {
-			err = copyFile(srcPath, dstPath)
-			if err != nil {
-				return
-			}
-		}
-	}
-
-	return
-}
-
-func copyFile(src, dst string) (err error) {
-	in, err := os.Open(src)
-	if err != nil {
-		return
-	}
-	defer in.Close()
-
-	out, err := os.Create(dst)
-	if err != nil {
-		return
-	}
-	defer func() {
-		if e := out.Close(); e != nil {
-			err = e
-		}
-	}()
-
-	_, err = io.Copy(out, in)
-	if err != nil {
-		return
-	}
-
-	err = out.Sync()
-	if err != nil {
-		return
-	}
-
-	si, err := os.Stat(src)
-	if err != nil {
-		return
-	}
-
-	err = os.Chmod(dst, si.Mode())
-	if err != nil {
-		return
-	}
-
-	return
+	OverrideEnv("SystemDrive", App.DataPath)
+	OverrideEnv("ProgramData", PathJoin(App.DataPath, "ProgramData"))
+	OverrideEnv("ALLUSERSPROFILE", PathJoin(App.DataPath, "ProgramData"))
+	OverrideEnv("USERPROFILE", PathJoin(App.DataPath, "UserProfile"))
+	OverrideEnv("APPDATA", PathJoin(App.DataPath, "UserProfile", "Roaming"))
+	Launch()
 }
